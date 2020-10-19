fe4d21619a9d9516bae124b4fd5a0f812dc6461a
Saul Florez <me@saulflorez.com>
2020-10-19 18:52:00 -0400

    📦 NEW: Clase #1 - Instalando gatsby

---
+++ src/components/jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledJumbo } from '../styles/components'
+
+export default function jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>¡Consigue el mejor swag exclusivo espacial de Platzi!</h2>
+        <small>Piezas elegantes para los mejores estudiantes.</small>
+      </div>
+    </StyledJumbo>
+  )
+}
