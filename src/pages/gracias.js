39741439b9d9bd719de99a90845ec90c1029f3bb
Saul Florez <me@saulflorez.com>
2020-10-19 21:17:38 -0400

    📦 NEW: Class #2 - Página de gracias

---
+++ src/pages/gracias.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import {  SEO } from "../components"
+import { Button, Purchase } from "../styles/components"
+
+const Gracias = () => (
+  <div>
+    <SEO title="Compra exitosa" />
+    <Purchase>
+      <h2>Compra Exitosa</h2>
+      <p>Expero que disfrutes tu swag, lucelo con or</p>
+      <p>¡Te esperamos de vuelta, no pares de aprender</p>
+      <span role="img" aria-label="emoji">♥</span>
+      <Link to="/">
+        <Button>Volver al Cátalogo</Button>
+      </Link>
+    </Purchase>
+  </div>
+)
+
+export default Gracias
