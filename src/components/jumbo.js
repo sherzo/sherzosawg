641dcf1639ce37c7f6e29dd32624ccdb78885bd4
Saul Florez <me@saulflorez.com>
2020-11-02 18:24:02 -0400

    📦 NEW: Class #4 - Consulta de gql

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StyledJumbo } from '../styles/components'
 
-export default function jumbo() {
+export default function jumbo({description}) {
   return (
     <StyledJumbo>
       <div>
         <h2>¡Consigue el mejor swag exclusivo espacial de Platzi!</h2>
-        <small>Piezas elegantes para los mejores estudiantes.</small>
+        <small>{description}</small>
       </div>
     </StyledJumbo>
   )
