8699c6cfd633e419c5f4af0f620fe09f3d6686e1
Saul Florez <me@saulflorez.com>
2020-11-05 21:24:19 -0400

    📦 NEW: Class #5 - Instalando y config. plugin

---
+++ src/utils/typography.js
@@ -0,0 +1,14 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    'Lato',
+    'Helvetica',
+    'Arial'
+  ],
+  bodyFontFamily: ['Open Sans', 'Roboto', 'Geogia']
+})
+
+export default typography;
