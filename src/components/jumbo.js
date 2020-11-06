3bea674235968812ef27aaeccc365d948ee3bee5
Saul Florez <me@saulflorez.com>
2020-11-05 21:41:29 -0400

    📦 NEW: Class #6 - Imagenes con gatsby

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { StyledJumbo } from '../styles/components'
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+import { Image } from "./"
 
-export default function jumbo({description}) {
+export default function jumbo({ description }) {
   return (
     <StyledJumbo>
       <div>
         <h2>¡Consigue el mejor swag exclusivo espacial de Platzi!</h2>
         <small>{description}</small>
       </div>
+      <Image name="icon" />
     </StyledJumbo>
   )
 }
